6661303bd9ab0ae937e776daba4c8bc10bcaf5de
leforyou <1147724499@qq.com>
2019-03-11 16:27:40 +0800

    初始化项目

---
+++ 前端/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="main-width">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.main-width{
+    width: 100%;
+    max-width: 10rem;
+    margin: 0 auto;
+}
+</style>
+
+<style lang="scss">
+@import "./../static/scss/reset.scss";
+</style>
+<style lang="scss" >
+@import "./../static/scss/main.scss";
+</style>
---
+++ 前端/src/page/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-page">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: '首页'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.home-page{
+  
+}
+</style>
